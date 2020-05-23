85f81f8428088641c0fb5c9679348c6b1185d42a
caravan4eg <alex.apanasik@gmail.com>
2020-05-23 17:32:25 +0300

    access restricted by authenticated users

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -24,6 +26,6 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-
+@login_required
 def profile(request):
     return render(request, 'users/profile.html',)
