30cd42bbe4e754ddca8cd3f22db342eab4d106e3
caravan4eg <alex.apanasik@gmail.com>
2020-05-27 20:11:28 +0300

    crud and authentication implemented

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
-from blog.views import home, about
+from blog.views import (
+    home,
+    about,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
+
 
 urlpatterns = [
-    path('', home, name='blog-home'),
+    # path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', about, name='blog-about'),
 ]
